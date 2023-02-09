5e773f4f9a452e265f92608506a23e474442f3ac
Ishav Sohal <ishav.sohal@mail.utoronto.ca>
2023-02-09 15:01:05 -0500

    Created component classes for App and AddItem display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
-function App() {
-  return (
-    <>Test</>
-  )
+class App extends React.Component{
+
+  constructor(props: {} | Readonly<{}>){
+    super(props);
+    this.state = {showMenu: true};
+  }
+
+  toggleMenu(){
+    this.setState({showMenu: !this.state.showMenu});
+  }
+
+  render() {
+    return(
+      <div id="root">
+        <button id="addItem" onClick={this.toggleMenu}> Add Item</button>
+      
+      </div>
+    )
+  }
 }
 
 export default App;
