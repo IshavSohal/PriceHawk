cd4effc908de74adec0ec4396c61d47b80f15de3
Justin Kidd <me@potatodiet.ca>
2023-02-09 20:45:54 -0500

    Complete register form functionality. (#12)

--- frontend/src/components/SignInButton.tsx
+++ frontend/src/components/SignInButton.tsx
@@ -28,94 +28,90 @@ export default function SignInButton() {
   };
 
   return (
-    <div>
-    <Stack direction="column" divider={<Divider orientation="horizontal" flexItem />} spacing={1}>
-      
-      <Button variant="contained" color="success" onClick={handleClickOpenSignIn}>
-        Sign In
-      </Button>
-      <Button variant="text" onClick={handleClickOpenRegistration}>
-        Registration
-      </Button>
-    
-    </Stack>
+    <>
+      <Stack direction="column" divider={<Divider orientation="horizontal" flexItem />} spacing={1}>
+        <Button variant="contained" color="success" onClick={handleClickOpenSignIn}>
+          Sign In
+        </Button>
+        <Button variant="text" onClick={handleClickOpenRegistration}>
+          Registration
+        </Button>
+      </Stack>
 
-    {/*MODAL FOR SIGN IN*/}
-    <Dialog open={openSignIn} onClose={handleClose}>
-        <DialogTitle>Sign In</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="email"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            margin="dense"
-            id="password"
-            label="Password"
-            type="password"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Sign In</Button>
-        </DialogActions>
-      </Dialog>
-    {/*MODAL FOR SIGN IN END*/}
+      {/*MODAL FOR SIGN IN*/}
+      <Dialog open={openSignIn} onClose={handleClose}>
+          <DialogTitle>Sign In</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="email"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              margin="dense"
+              id="password"
+              label="Password"
+              type="password"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Sign In</Button>
+          </DialogActions>
+        </Dialog>
+      {/*MODAL FOR SIGN IN END*/}
 
-    {/*MODAL FOR REGISTRATION*/}
-    <Dialog open={openRegistration} onClose={handleClose}>
-        <DialogTitle>Registration</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="email"
-            label="Enter Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            margin="dense"
-            id="email"
-            label="Re-Enter Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
+      {/*MODAL FOR REGISTRATION*/}
+      <Dialog open={openRegistration} onClose={handleClose}>
+          <DialogTitle>Registration</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="email"
+              label="Enter Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              margin="dense"
+              id="email"
+              label="Re-Enter Email Address"
+              type="email"
+              fullWidth
+              variant="standard"
+            />
 
-          <TextField
-            margin="dense"
-            id="password"
-            label="Enter Password"
-            type="password"
-            fullWidth
-            variant="standard"
-          />
-          <TextField
-            margin="dense"
-            id="password"
-            label="Re-enter Password"
-            type="password"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose}>Submit</Button>
-        </DialogActions>
-      </Dialog>
-    {/*MODAL FOR REGISTRATION END*/}
-
-    </div>
-    
+            <TextField
+              margin="dense"
+              id="password"
+              label="Enter Password"
+              type="password"
+              fullWidth
+              variant="standard"
+            />
+            <TextField
+              margin="dense"
+              id="password"
+              label="Re-enter Password"
+              type="password"
+              fullWidth
+              variant="standard"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleClose}>Submit</Button>
+          </DialogActions>
+        </Dialog>
+      {/*MODAL FOR REGISTRATION END*/}
+    </>
   );
 }
--- frontend/src/popup.tsx
+++ frontend/src/popup.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { MemoryRouter } from 'react-router';
 import App from './App';
-import TrackingPageButton from './components/TrackingPageButton'
-import SignInButton from './components/SignInButton'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <div>
   <React.StrictMode>
-    <App />
-    <SignInButton></SignInButton>
-    {/* <TrackingPageButton> </TrackingPageButton> */}
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
   </React.StrictMode>
-  
-  </div>
 );
