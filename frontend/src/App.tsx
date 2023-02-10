9f221cb53991b26b11dc33433c08a3aecdf44932
drosari3 <drosari3@mail.utoronto.ca>
2023-02-09 20:10:36 -0500

    Added final code for the logic for sso logins.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import { useState } from "react";
+import googleSignIn from "./oauth";
 
 function App() {
-  return (
-    <>Test</>
-  )
+    const [user, setUser] = useState('')
+    return (<>
+        {user ? <p>Hi! {user}</p> : <button id="google-login" onClick={googleSignIn(setUser)}>Sign In With Google</button>}
+    </>
+    )
 }
 
 export default App;
