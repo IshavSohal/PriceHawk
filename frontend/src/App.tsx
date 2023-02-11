fc7ae0419bf268da3cf89a1504035a9ae9792f60
drosari3 <drosari3@mail.utoronto.ca>
2023-02-10 21:40:17 -0500

    Minor Bug with not putting () in onCLick.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import googleSignIn from "./oauth";
+import googleSignIn from "./googleSignIn";
 
 function App() {
     const [user, setUser] = useState('')
     return (<>
-        {user ? <p>Hi! {user}</p> : <button id="google-login" onClick={googleSignIn(setUser)}>Sign In With Google</button>}
+        {user ? <p>Hi! {user}</p> : <button id="google-login" onClick={() => googleSignIn(setUser)}>Sign In With Google</button>}
     </>
     )
 }
