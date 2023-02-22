861eca17bc2eedd65d0450e7a4567b3aaadff226
SyedTahaA <62918944+SyedTahaA@users.noreply.github.com>
2023-02-22 16:55:59 -0500

    Fix Auth Token not in Add Item Request Header (#24)
    - Add auth token for signed in user to add item request header
    - Refactor getToken code to utilities folder

---
+++ frontend/src/utilities/session.ts
@@ -0,0 +1,13 @@
+/**
+ * This file should contain all user session related
+ * helper/utility methods.
+ */
+
+/**
+ * Get current user
+ * @returns logged in users auth token
+ */
+export async function getToken() {
+  const data = await chrome.storage.local.get(["token"]);
+  return data.token;
+}
