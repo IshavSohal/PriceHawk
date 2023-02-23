17774c97db2f6c31b5ce6a499fe1f27f27a24a2d
SyedTahaA <awesome.taha.47@gmail.com>
2023-02-23 18:43:24 -0500

    Add html fields to Item model and serializer

--- backend/items/serializers/Items.py
+++ backend/items/serializers/Items.py
@@ -10,10 +10,12 @@ class ItemSerializer(serializers.ModelSerializer):
     to help with requests.
     """
     guestid = serializers.CharField(write_only=True, required=False)
+    name_html = serializers.CharField(required=False)
+    price_html = serializers.CharField(required=False)
 
     class Meta:
         model = Item
-        fields = ['id', 'name', 'price', 'url', 'created', 'guestid']
+        fields = ['id', 'name', 'price', 'url', 'name_html', 'price_html', 'created', 'guestid']
         extra_kwargs = {'created': {'read_only': True}, 'guestid': {'write_only': True}}
 
     def validate_price(self, value):
@@ -47,6 +49,7 @@ class ItemSerializer(serializers.ModelSerializer):
         request = self.context.get('request', None)
         user = request.user
         name, price, url = validated_data.get('name'), validated_data.get('price'), validated_data.get('url')
+        n_html, p_html = validated_data.get('name_html', None), validated_data.get('price_html', None)
 
         if user.is_authenticated:
             if Item.objects.filter(user=user, name=name, url=url).exists():
@@ -56,11 +59,13 @@ class ItemSerializer(serializers.ModelSerializer):
                 user = user,
                 name = name,
                 price = price,
-                url = url
+                url = url,
+                name_html = n_html,
+                price_html = p_html
             )
         
         
-        guest = validated_data.get('guestid')
+        guest = validated_data.get('guestid', None)
         if not guest:
             raise PermissionDenied("Guest ID must be provided if not signed in", 403)
         if Item.objects.filter(guest_session=guest, name=name, url=url).exists():
@@ -69,5 +74,7 @@ class ItemSerializer(serializers.ModelSerializer):
                 guest_session = guest,
                 name = name,
                 price = price,
-                url = url
+                url = url,
+                name_html = n_html,
+                price_html = p_html
             )
