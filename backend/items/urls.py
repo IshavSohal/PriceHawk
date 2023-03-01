40c0edc59db22e64cf18f46c5204fb4b66dbdb59
Ovidiu Bodean <ovidiu.bodean@mail.utoronto.ca>
2023-03-01 14:27:27 -0500

    added backend functionality for user story 1 that allows a user to fetch all their currently tracked items. Added starter code for frontend: button that opens a user's tracking page (plain for now)

--- backend/items/urls.py
+++ backend/items/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from items.views.ItemsView import *
-
+from items.views.GetItemsView import *
 
 app_name = 'items'
 
 urlpatterns = [
     path('create/', CreateItemsView.as_view(), name='add-item'),
+    path('get-items/', GetUserItemsView.as_view(), name='get-items')
 ]
