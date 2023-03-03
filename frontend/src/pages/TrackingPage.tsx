67b328677839f6b6bc4d304533c1b2d3ab24c9cb
Mustafa <itsmustafairfan@gmail.com>
2023-03-03 13:02:46 -0500

    Added Delete item popup

--- frontend/src/pages/TrackingPage.tsx
+++ frontend/src/pages/TrackingPage.tsx
@@ -11,9 +11,15 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { getToken } from '../utilities/session';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 const TrackingPage = () => {
   const [dataNew, setDataNew] = useState<any[]>([]);
+  const [open, setOpen] = useState(false);
   useEffect(() => {
     // axios.get("http://localhost:8000/items/get-items/").then((data) => setDataNew(data.data))
     async function getData() {
@@ -28,7 +34,7 @@ const TrackingPage = () => {
   }, []);
   // axios.delete(`http://localhost:8000/items/delete-item/${dataNew}/`)
 
-  function handleDelete(id: any) {
+  function handleDelete(id: any, dataArray: any) {
     async function deleteItem() {
       await fetch(`http://localhost:8000/items/delete-item/${id}/`, {
         method: "DELETE",
@@ -37,8 +43,18 @@ const TrackingPage = () => {
         }
       });
     }
+    let index = dataArray.findIndex((obj: any) => obj.id === id)
+    dataArray.splice(index, 1);
     deleteItem();
   }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <TableContainer component={Paper}>
@@ -65,7 +81,30 @@ const TrackingPage = () => {
               <TableCell align="right">{row.name}</TableCell>
               <TableCell align="right">{row.price}</TableCell>
               {/* <TableCell align="right">{row.created}</TableCell> */}
-              <Button onClick ={() => handleDelete(row.id)}>Delete</Button>
+              <Button onClick ={handleClickOpen}>Delete</Button>
+
+              <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Delete item?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Do you permanently want to delete this item from the tracking page?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Disagree</Button>
+          <Button onClick={() => handleDelete(row.id, dataNew)} autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+
             </TableRow>
           ))}
         </TableBody>
