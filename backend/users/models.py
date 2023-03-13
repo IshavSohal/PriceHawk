037b06108faa8b93f925e558276c359db5927c0d
Aoroto <xgenerationshimon@gmail.com>
2023-03-13 15:09:59 -0400

    Added source for models.py.

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+'''
+https://stackoverflow.com/questions/65416042/max-and-min-values-for-a-django-model-field-according-to-the-values-already-int
+https://stackoverflow.com/questions/42425933/how-do-i-set-a-default-max-and-min-value-for-an-integerfield-django
+for how to get the validators
+'''
+
 class PHUser(AbstractUser):
     emailnotifications = models.BooleanField(default = False)
     priceInterval = models.PositiveIntegerField(default = 12, validators = [MinValueValidator(1), MaxValueValidator(24)])
